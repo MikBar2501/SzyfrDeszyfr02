e7348ab6442920080882bc92ea0e0bb33aa3143f
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2019-04-16 00:49:24 +0200

    New Version

---
+++ SzyfrDeszyfr/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SzyfrDeszyfr
+{
+    partial class SzyfrowanieDeszyfrowanie
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSzyfr = new System.Windows.Forms.Button();
+            this.btnDeszyfr = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSzyfr
+            // 
+            this.btnSzyfr.Location = new System.Drawing.Point(12, 104);
+            this.btnSzyfr.Name = "btnSzyfr";
+            this.btnSzyfr.Size = new System.Drawing.Size(121, 23);
+            this.btnSzyfr.TabIndex = 0;
+            this.btnSzyfr.Text = "Szyfrowanie";
+            this.btnSzyfr.UseVisualStyleBackColor = true;
+            this.btnSzyfr.Click += new System.EventHandler(this.BtnSzyfr_Click);
+            // 
+            // btnDeszyfr
+            // 
+            this.btnDeszyfr.Location = new System.Drawing.Point(139, 104);
+            this.btnDeszyfr.Name = "btnDeszyfr";
+            this.btnDeszyfr.Size = new System.Drawing.Size(123, 23);
+            this.btnDeszyfr.TabIndex = 1;
+            this.btnDeszyfr.Text = "Deszyfrowanie";
+            this.btnDeszyfr.UseVisualStyleBackColor = true;
+            this.btnDeszyfr.Click += new System.EventHandler(this.BtnDeszyfr_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ścieżka do pliku:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 30);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(250, 20);
+            this.txtPath.TabIndex = 3;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(12, 78);
+            this.txtKey.MaxLength = 11;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(250, 20);
+            this.txtKey.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Klucz:";
+            // 
+            // SzyfrowanieDeszyfrowanie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 139);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeszyfr);
+            this.Controls.Add(this.btnSzyfr);
+            this.Name = "SzyfrowanieDeszyfrowanie";
+            this.Text = "SzyfrowanieDeszyfrowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSzyfr;
+        private System.Windows.Forms.Button btnDeszyfr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SzyfrDeszyfr/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace SzyfrDeszyfr
+{
+    public partial class SzyfrowanieDeszyfrowanie : Form
+    {
+
+        //C:\Users\mikol\Desktop\PlikTestowy.txt
+        //C:\Users\mikol\Desktop\TestFile.txt
+        //C:\Users\mikol\Desktop\TestingFile.txt
+        //C:\Users\mikol\Desktop\tescikDesz.txt
+        //C:\Users\mikol\Desktop\tescikDesz — kopia - deszeszeszeszeszeszeszeszeszesze.txt
+        public string path;
+        public string fileName;
+
+        public SzyfrowanieDeszyfrowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSzyfr_Click(object sender, EventArgs e)
+        {
+            SetNameAndPath(txtPath.Text,"zaszyfrowane");
+            if(Encryption(txtPath.Text))
+            {
+                MessageBox.Show("Zaszyfrowano plik", "Zaszyfrowano", MessageBoxButtons.OK);
+            } else
+            {
+                MessageBox.Show("Błąd przy szyfrowaniu pliku", "Błąd", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void BtnDeszyfr_Click(object sender, EventArgs e)
+        {
+            SetNameAndPath(txtPath.Text,"odszyfrowane");
+            
+            if (Decryption(txtPath.Text))
+            {
+                MessageBox.Show("Odszyfrowano plik", "Odszyfrowano", MessageBoxButtons.OK);
+            }
+            else
+            {
+                MessageBox.Show("Błąd przy deszyfrowaniu pliku", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void SetNameAndPath(string text, string add)
+        {
+            string[] pathOrginal = text.Split('\\');
+
+            for(int i = 0; i < pathOrginal.Length - 1; i++)
+            {
+                path += pathOrginal[i] + "\\";
+            }
+
+            string[] nameOriginal = pathOrginal[pathOrginal.Length - 1].Split('.');
+            nameOriginal[0] += " ";
+            nameOriginal[0] += add;
+
+            fileName = nameOriginal[0] + "." + nameOriginal[1];
+        }
+
+
+        string CreateKey(string preKey)
+        {
+            string key = "";
+            CultureInfo culture = new CultureInfo("pl-PL");
+            if (preKey.Length < 11)
+            {
+                int countTo11 = 11 - preKey.Length;
+                for (int i = 0; i < countTo11; i++)
+                {
+                    preKey += "0";
+                }
+
+            }
+
+            for (int i = 0; i < preKey.Length; i++)
+            {
+                string outHex = "0";
+                int b = Convert.ToInt32(preKey[i], culture);
+                string hex = b.ToString("x");
+                if (hex.Length > 2)
+                {
+                    outHex = hex;
+                }
+                else
+                {
+                    outHex += hex;
+                }
+                key += outHex;
+            }
+
+            string lastKey = "";
+            for (int i = 0; i < key.Length - 1; i++)
+            {
+                lastKey += key[i];
+            }
+
+            return lastKey;
+        }
+
+        public bool Encryption(string file) 
+        {
+            CultureInfo culture = new CultureInfo("pl-PL");
+            try
+            {
+                string[] lines = File.ReadAllLines(file, Encoding.UTF8);
+                string outText = "";
+                foreach (string line in lines)
+                {
+                    foreach (char sign in line)
+                    {
+                        string outHex = "00";
+                        int b = Convert.ToInt32(sign, culture);
+                        string hex = b.ToString("x");
+                        if (hex.Length > 4)
+                        {
+                            return false;
+                        }
+                        else if (hex.Length == 4)
+                        {
+                            outHex = hex;
+                        }
+                        else if (hex.Length == 3)
+                        {
+                            outHex = "0";
+                            outHex += hex;
+                        }
+                        else
+                        {
+                            outHex += hex;
+                        }
+                        outText += outHex;
+                    }
+                    outText += "0000";
+                }
+
+                using (StreamWriter outputFile = new StreamWriter(path + fileName, false, Encoding.UTF8))
+                {
+                    outputFile.WriteLine(outText);
+                    outputFile.Close();
+                }
+
+                return true;
+            }
+            catch (FileNotFoundException exnotfound)
+            {
+                MessageBox.Show("Nie znaleziono ścieżki", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public bool Decryption(string file)
+        {
+            CultureInfo culture = new CultureInfo("pl-PL");
+            try
+            {
+                string[] lines = File.ReadAllLines(file, Encoding.UTF8);
+                string outText = "";
+                foreach (string line in lines)
+                {
+                    outText += line;
+                }
+
+                if (outText.Length % 4 != 0)
+                {
+                    return false;
+                    //MessageBox.Show("Error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    using (StreamWriter outputFile = new StreamWriter(path + fileName, false, Encoding.UTF8))
+                    {
+                        for (int i = 0; i < outText.Length; i += 4)
+                        {
+                            if (outText[i].Equals('0') && outText[i + 1].Equals('0') && outText[i + 2].Equals('0') && outText[i + 3].Equals('0'))
+                            {
+                                outputFile.WriteLine();
+                            }
+                            else
+                            {
+                                string sign = "";
+                                sign += outText[i];
+                                sign += outText[i + 1];
+                                sign += outText[i + 2];
+                                sign += outText[i + 3];
+                                int hex = Int32.Parse(sign, System.Globalization.NumberStyles.HexNumber);
+                                outputFile.Write(Convert.ToChar(hex, culture));
+                            }
+                        }
+                        outputFile.Close();
+                        return true;
+                    }
+                    
+                }
+            }
+            catch (FileNotFoundException exnotfound)
+            {
+                MessageBox.Show("Nie znaleziono ścieżki", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+    }
+}
