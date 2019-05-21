ce6cd167d9c642539b730c2ed9293e7e453095da
Emejpi <emejpi@gmail.com>
2019-05-21 11:19:55 +0200

    poprawa polskich znakow

--- SzyfrDeszyfr/Form1.cs
+++ SzyfrDeszyfr/Form1.cs
@@ -20,36 +20,37 @@ namespace SzyfrDeszyfr
         //C:\Users\mikol\Desktop\TestingFile.txt
         //C:\Users\mikol\Desktop\tescikDesz.txt
         //C:\Users\mikol\Desktop\tescikDesz — kopia - deszeszeszeszeszeszeszeszeszesze.txt
-        public string path;
-        public string fileName;
+        public String path;
+        public String fileName;
 
         public bool AES;
         public const int miniLength = 4;
         public const int AESLength = 32;
-        string hex;
+        String hex;
 
         public SzyfrowanieDeszyfrowanie()
         {
             InitializeComponent();
+            //Test();
         }
 
-        string GetKey()
+        String GetKey()
         {
             return CreateKey(txtKey.Text);
         }
 
         private void BtnSzyfr_Click(object sender, EventArgs e)
         {
-            if((AES && txtKey.Text.Length == AESLength) || (!AES && txtKey.Text.Length == miniLength))
+            if ((AES && txtKey.Text.Length == AESLength) || (!AES && txtKey.Text.Length == miniLength))
             {
                 //SetNameAndPath(txtPath.Text, "zaszyfrowane");
-                
+
                 if (LoadAndHex(txtPath.Text, out hex))
                 {
                     Console.WriteLine(hex);
 
-                    string cypher = TrueEncryption(hex, GetKey());
-                    SetNameAndPath(txtPath.Text, "zahexowane");
+                    String cypher = TrueEncryption(hex, GetKey());
+                    SetNameAndPath(txtPath.Text, "zaszyfrowane");
                     using (StreamWriter outputFile = new StreamWriter(path + fileName, false, Encoding.UTF8))
                     {
                         outputFile.WriteLine(cypher);
@@ -78,19 +79,19 @@ namespace SzyfrDeszyfr
         {
             if ((AES && txtKey.Text.Length == AESLength) || (!AES && txtKey.Text.Length == miniLength))
             {
-                string[] lines = File.ReadAllLines(txtPath.Text, Encoding.UTF8);
-                string hexToEncryption = "";
-                foreach (string line in lines)
+                String[] lines = File.ReadAllLines(txtPath.Text, Encoding.UTF8);
+                String hexToEncryption = "";
+                foreach (String line in lines)
                 {
                     hexToEncryption += line;
                 }
 
-                string text = TrueDecryption(hexToEncryption, GetKey());
+                String text = TrueDecryption(hexToEncryption, GetKey());
 
                 //if (SaveAndUnhex(txtPath.Text))
                 if (SaveAndUnhex(text))
                 {
-                    
+
                     /*if (!SaveAndUnhex(text))
                     {
                         MessageBox.Show("Błąd przy zapisie pliku", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -103,15 +104,16 @@ namespace SzyfrDeszyfr
                 {
                     MessageBox.Show("Błąd przy deszyfrowaniu pliku", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-            } else
+            }
+            else
             {
                 MessageBox.Show("Nieprawidłowa ilość znaków w kluczu", "Błąd", MessageBoxButtons.OK);
             }
         }
 
-        public void SetNameAndPath(string text, string add)
+        public void SetNameAndPath(String text, String add)
         {
-            string[] pathOrginal = text.Split('\\');
+            String[] pathOrginal = text.Split('\\');
 
             path = "";
             for (int i = 0; i < pathOrginal.Length - 1; i++)
@@ -119,7 +121,7 @@ namespace SzyfrDeszyfr
                 path += pathOrginal[i] + "\\";
             }
 
-            string[] nameOriginal = pathOrginal[pathOrginal.Length - 1].Split('.');
+            String[] nameOriginal = pathOrginal[pathOrginal.Length - 1].Split('.');
             nameOriginal[0] += " ";
             nameOriginal[0] += add;
 
@@ -127,16 +129,16 @@ namespace SzyfrDeszyfr
         }
 
 
-        string CreateKey(string preKey)
+        String CreateKey(String preKey)
         {
-            string key = "";
+            String key = "";
             CultureInfo culture = new CultureInfo("pl-PL");
 
             for (int i = 0; i < preKey.Length; i++)
             {
-                string outHex = "0";
+                String outHex = "0";
                 int b = Convert.ToInt32(preKey[i], culture);
-                string hex = b.ToString("x");
+                String hex = b.ToString("x");
                 if (hex.Length > 2)
                 {
                     outHex = hex;
@@ -150,7 +152,7 @@ namespace SzyfrDeszyfr
             return key;
         }
 
-        string TrueEncryption(string hex, string key)
+        String TrueEncryption(String hex, String key)
         {
             Cypher cypher = new Cypher();
             if (AES)
@@ -159,7 +161,7 @@ namespace SzyfrDeszyfr
             return cypher.MiniAESEncryption(hex, key);
         }
 
-        string TrueDecryption(string hex, string key)
+        String TrueDecryption(String hex, String key)
         {
             Cypher cypher = new Cypher();
             if (AES)
@@ -168,21 +170,122 @@ namespace SzyfrDeszyfr
             return cypher.MiniAESDecryption(hex, key);
         }
 
+        public void Test()
+        {
+            //String test = "ąćżźęńó";
 
-        public bool LoadAndHex(string file, out string outText) 
+            //SaveTest(test);
+            String test = LoadTest();
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                Char ch = test[i];
+                String hex = HexChar(ch);
+                //SaveTest(hex);
+                //String loadedHex = LoadTest();
+                Char afterCh = CharHex(hex);
+
+                Console.WriteLine(ch + " -> " + hex + " -> " + afterCh);
+            }
+        }
+
+        public void SaveTest(String text)
+        {
+            //SetNameAndPath("");
+            using (StreamWriter outputFile = new StreamWriter("C:\\Users\\Emejpi\\Desktop\\Char.txt", false, Encoding.UTF8))
+            {
+                outputFile.WriteLine(text);
+                outputFile.Close();
+            }
+        }
+
+        public String LoadTest()
+        {
+            String[] lines = File.ReadAllLines("C:\\Users\\Emejpi\\Desktop\\Char.txt", GetEncoding("C:\\Users\\Emejpi\\Desktop\\Char.txt"));
+            return lines[0];
+        }
+
+        private static Encoding GetEncoding(string filename)
+        {
+            using (var reader = new StreamReader(filename, Encoding.Default, true))
+            {
+                if (reader.Peek() >= 0)
+                    reader.Read();
+
+                return reader.CurrentEncoding;
+            }
+        }
+
+        public Char CharHex(String hex)
+        {
+            CultureInfo culture = new CultureInfo("pl-PL");
+
+            int i = 0;
+            String outText = hex;
+
+            if (outText[i].Equals('0') && outText[i + 1].Equals('0') && outText[i + 2].Equals('0') && outText[i + 3].Equals('0'))
+            {
+                return '\n';
+            }
+            else
+            {
+                String sign = "";
+                sign += outText[i];
+                sign += outText[i + 1];
+                sign += outText[i + 2];
+                sign += outText[i + 3];
+                int number = Int32.Parse(sign, System.Globalization.NumberStyles.HexNumber);
+                return Convert.ToChar(number, culture);
+            }
+        }
+
+        public String HexChar(Char sign)
+        {
+            CultureInfo culture = new CultureInfo("pl-PL");
+
+            String outHex = "";
+            int b = Convert.ToInt32(sign, culture);
+            String hex = b.ToString("x");
+            if (hex.Length > 4)
+            {
+                return "XXXX";
+            }
+            else if (hex.Length == 4)
+            {
+                outHex = hex;
+            }
+            else if (hex.Length == 3)
+            {
+                outHex = "0";
+                outHex += hex;
+            }
+            else if (hex.Length == 2)
+            {
+                outHex = "00";
+                outHex += hex;
+            }
+            else
+            {
+                outHex = "000";
+                outHex += hex;
+            }
+            return outHex;
+        }
+
+        public bool LoadAndHex(String file, out String outText)
         {
             CultureInfo culture = new CultureInfo("pl-PL");
             try
             {
-                string[] lines = File.ReadAllLines(file, Encoding.UTF8);
+                String[] lines = File.ReadAllLines(file, GetEncoding(file));
                 outText = "";
-                foreach (string line in lines)
+                foreach (String line in lines)
                 {
-                    foreach (char sign in line)
+                    foreach (Char sign in line)
                     {
-                        string outHex = "";
+                        String outHex = "";
                         int b = Convert.ToInt32(sign, culture);
-                        string hex = b.ToString("x");
+                        String hex = b.ToString("x");
                         if (hex.Length > 4)
                         {
                             return false;
@@ -196,11 +299,12 @@ namespace SzyfrDeszyfr
                             outHex = "0";
                             outHex += hex;
                         }
-                        else if(hex.Length == 2)
+                        else if (hex.Length == 2)
                         {
                             outHex = "00";
                             outHex += hex;
-                        } else
+                        }
+                        else
                         {
                             outHex = "000";
                             outHex += hex;
@@ -209,7 +313,7 @@ namespace SzyfrDeszyfr
                     }
                     outText += "0000";
                 }
-                
+
 
                 return true;
             }
@@ -221,19 +325,19 @@ namespace SzyfrDeszyfr
             }
         }
 
-        public bool SaveAndUnhex(string hextext)
+        public bool SaveAndUnhex(String hextext)
         {
             CultureInfo culture = new CultureInfo("pl-PL");
             try
             {
-                //string[] lines = File.ReadAllLines(file, Encoding.UTF8);
+                //String[] lines = File.ReadAllLines(file, Encoding.UTF8);
                 //outText = "";
-                //foreach (string line in lines)
+                //foreach (String line in lines)
                 //{
                 //    outText += line;
                 //}
 
-                string outText = hextext;
+                String outText = hextext;
 
                 if (outText.Length % 4 != 0)
                 {
@@ -253,7 +357,7 @@ namespace SzyfrDeszyfr
                             }
                             else
                             {
-                                string sign = "";
+                                String sign = "";
                                 sign += outText[i];
                                 sign += outText[i + 1];
                                 sign += outText[i + 2];
@@ -265,7 +369,7 @@ namespace SzyfrDeszyfr
                         outputFile.Close();
                         return true;
                     }
-                    
+
                 }
             }
             catch (FileNotFoundException exnotfound)
